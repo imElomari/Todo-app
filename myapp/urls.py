fb2a803605ca87ddb4841b309855869caffb3b15
imElomari <133535601+imElomari@users.noreply.github.com>
2024-08-18 21:06:24 +0100

    Delete myapp directory

--- myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.task_list, name='task_list'),
-    path('create-todo/', views.create_todo, name='create-todo'),
-    path('append-to-form/<int:pk>/', views.append_to_form, name='append-to-form'),
-    path('task/<int:pk>/delete/', views.delete_task, name='delete-task'),
-]
